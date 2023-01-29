2494c5df596fed92aec2160b93c09b809b083277
ed828a <ed828a@gmail.com>
2023-01-29 17:51:19 +1030

    mongoDB Arregation operations

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,21 +1,30 @@
-const express = require('express')
-const {getAllTours, createTour, getTour, updateTour, deleteTour, checkId, checkBody, aliasTopTours} = require('../controllers/tourController')
+const express = require("express");
+const {
+    getAllTours,
+    createTour,
+    getTour,
+    updateTour,
+    deleteTour,
+    checkId,
+    checkBody,
+    aliasTopTours,
+    getTourStats,
+    getMonthlyPlan,
+} = require("../controllers/tourController");
 
-
-const router = express.Router()
+const router = express.Router();
 
 // local middleware
 // router.param('id', checkId)
-router.param('id', (req, res, next, val) => {
-    console.log('val', val)
-    next()
-})
+router.param("id", (req, res, next, val) => {
+    console.log("val", val);
+    next();
+});
 
-router.route('/top-5-cheap').get(aliasTopTours, getAllTours)
+router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
+router.route("/tour-stats").get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan)
 router.route("/").get(getAllTours).post(checkBody, createTour);
-router.route("/:id")
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour);
+router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
-module.exports = router
+module.exports = router;
